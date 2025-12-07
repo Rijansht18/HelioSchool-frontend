3915df54c72620a346fa16afe216b185d5f240f4
Rijansht18 <rijan0518@gmail.com>
2025-12-08 00:09:29 +0545

    update

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../styles/common/footer.css'
+import '../../styles/common/Footer.css'
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
